b4f033ea7440cab11028de7b54cf64cd633692b9
faixan554 <faixan.malik554@gmail.com>
2022-12-13 21:43:25 +0500

    add add lists link in tshirt.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Hero from '../components/Hero'
-import Products from '../components/Products'
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
       </Head>
 
       <Hero/>
-      <Products/>
 
       
       
