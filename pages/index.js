9277132b8b4bc80238a266bbe531a7576b999c30
faixan554 <faixan.malik554@gmail.com>
2022-12-13 16:36:42 +0500

    setup tailwind css with next js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hunting_Store</title>
+        <meta name="description" content="Hunting_Store" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  )
+}
