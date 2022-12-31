de455fcc793e4c2f56a32a5ac0bc96aaa4c7a0b5
faixan554 <faixan.malik554@gmail.com>
2022-12-31 17:27:18 +0500

    complete tshirt page

--- .history/pages/tshirt_20221231172649.js
+++ .history/pages/tshirt_20221231172649.js
@@ -12,9 +12,6 @@ function Tshirt({ product }) {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-
-
-        {Object.keys(product).length === 0 && <div className="font-semibold mx-auto">Sorry! Currently Stock Unavailble right now. Please wait for the new Stock.!</div>}  
         {Object.keys(product).map((item)=>{
             return <Link key={product[item]._id} href={`/product/${product[item].slug}`} className="group">
               <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
