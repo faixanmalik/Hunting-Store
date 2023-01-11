495fb5e8e4755e9e54d2e212056d6aae37d8fa32
faixan554 <faixan.malik554@gmail.com>
2023-01-11 14:30:26 +0500

    hero section complete ui design

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Hero () {
   return (
@@ -7,11 +8,10 @@ function Hero () {
         <h1 className='text-3xl sm:text-4xl md:text-6xl font-sans tracking-tight text-white text-center font-semibold'>New arrivals are here</h1>
         <p className='text-sm sm:text-base px-5 md:px-0 md:text-xl my-3 text-center text-gray-100'>The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release while they're still in stock.</p>
         <div className='mx-auto justify-center flex'>
-        <button className='bg-white md:mt-3 text-sm sm:text-base font-semibold rounded-md p-2 md:p-3 md:px-8'>Shop New Arrivals</button>
+          <button className='button px-3 sm:w-44 md:mt-3 text-sm sm:text-base font-semibold'>Shop New Arrivals</button>       
         </div>
       </div>
-      <img className='md:max-h-screen w-full' src="../image.jpg" alt="" />
-
+        <img className='w-full h-80 sm:h-500 md:h-650' src="../image4.jpg" alt="" />
     </div>
   )
 }
